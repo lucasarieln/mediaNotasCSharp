f0bd63cd06d9e25ebbeea5c1990545b2ee3e92f4
Lucas Ariel <lucasarieln@outlook.com>
2020-06-11 15:50:03 -0300

    primeiro código em C# - DIO

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace RevisaoLucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota: ");
+                        
+                            //CONVERTE NOTA
+                            if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                            {
+                                aluno.Nota = nota;
+                            }
+                            else
+                            {
+                                throw new ArithmeticException("Valor da nota deve ser decimal");
+                            }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        //TODO: listar alunos
+                        foreach(var a in alunos)
+                        {  
+                            if (!string.IsNullOrEmpty(a.Nome) && !a.Nota.Equals(""))
+                            {
+                            Console.WriteLine($"Aluno: {a.Nome} - Nota: {a.Nota}");
+                            }
+                        }
+
+                        break;
+                    case "3":
+                        //TODO: calcular media geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        ConceitoEnum conceitoGeral;
+
+                        if (mediaGeral < 3)
+                        {
+                            conceitoGeral = ConceitoEnum.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = ConceitoEnum.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = ConceitoEnum.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = ConceitoEnum.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = ConceitoEnum.A;
+                        }
+
+
+                        Console.WriteLine($"A média geral das notas é: {mediaGeral}, conceito: {conceitoGeral}");
+                        
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Valor informado incorreto");
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
